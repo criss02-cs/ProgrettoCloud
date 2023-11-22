2ae8122182ed8596dac781c4b0dc9445f42d52ce
Cristiano Aloigi <cristianoaloigi0@gmail.com>
2023-11-22 22:48:53 +0100

    sistemato dockerfile, e fatto refactoring

--- ProgettoCloud.Model/ResponseDto.cs
+++ ProgettoCloud.Model/ResponseDto.cs
@@ -1,3 +1,3 @@
 ﻿namespace ProgettoCloud.Model;
 
-public record ResponseDto(string CountryCode, string VatNumber, DateTime RequestDate, bool Valid, string Name, string Address);
+public record ResponseDto(string CountryCode, string VatNumber, DateTime RequestDate, bool IsValid, string Name, string Address);
